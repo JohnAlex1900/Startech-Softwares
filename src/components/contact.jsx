0b7aeae6f91bd7e65380576386c80394438983a6
Jules21-stack <gitongajules@gmail.com>
2024-03-15 21:26:09 +0300

    init commit

---
+++ src/components/contact.jsx
@@ -0,0 +1,165 @@
+import React, { useState, useEffect } from "react";
+import { useForm } from "@formspree/react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const initialState = {
+  name: "",
+  email: "",
+  message: "",
+};
+export const Contact = (props) => {
+  const [formData, setFormData] = useState(initialState);
+  const [state, handleSubmit] = useForm("mpzvjapz");
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  useEffect(() => {
+    if (state.succeeded) {
+      clearState();
+      window.scrollTo({ top: 0, behavior: "smooth" });
+      toast.success("Your message has been submitted successfully!");
+    }
+  }, [state.succeeded]);
+
+  const clearState = () => setFormData(initialState);
+
+  return (
+    <>
+      <div>
+        <div id="contact">
+          <div className="container">
+            <div className="col-md-8">
+              <div className="row">
+                <div className="section-title">
+                  <h2>Get In Touch</h2>
+                  <p>
+                    Please fill out the form below to send us an email and we
+                    will get back to you as soon as possible.
+                  </p>
+                </div>
+                <form name="sentMessage" onSubmit={handleSubmit}>
+                  <div className="row">
+                    <div className="col-md-6">
+                      <div className="form-group">
+                        <input
+                          type="text"
+                          id="name"
+                          name="name"
+                          className="form-control"
+                          placeholder="Name"
+                          required
+                          value={formData.name}
+                          onChange={handleChange}
+                        />
+                        <p className="help-block text-danger"></p>
+                      </div>
+                    </div>
+                    <div className="col-md-6">
+                      <div className="form-group">
+                        <input
+                          type="email"
+                          id="email"
+                          name="email"
+                          className="form-control"
+                          placeholder="Email"
+                          required
+                          value={formData.email}
+                          onChange={handleChange}
+                        />
+                        <p className="help-block text-danger"></p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="form-group">
+                    <textarea
+                      name="message"
+                      id="message"
+                      className="form-control"
+                      rows="4"
+                      placeholder="Message"
+                      required
+                      value={formData.message}
+                      onChange={handleChange}
+                    ></textarea>
+                    <p className="help-block text-danger"></p>
+                  </div>
+                  {/* <div id="success"></div> */}
+                  <button
+                    type="submit"
+                    className="btn btn-custom btn-lg"
+                    disabled={state.submitting}
+                  >
+                    {state.submitting ? "Sending..." : "Send Message"}
+                  </button>
+                </form>
+              </div>
+            </div>
+            <div className="col-md-3 col-md-offset-1 contact-info">
+              <div className="contact-item">
+                <h3>Contact Info</h3>
+                <p>
+                  <span>
+                    <i className="fa fa-map-marker"></i> Address
+                  </span>
+                  {props.data ? props.data.address : "loading"}
+                </p>
+              </div>
+              <div className="contact-item">
+                <p>
+                  <span>
+                    <i className="fa fa-phone"></i> Phone
+                  </span>{" "}
+                  {props.data ? props.data.phone : "loading"}
+                </p>
+              </div>
+              <div className="contact-item">
+                <p>
+                  <span>
+                    <i className="fa fa-envelope-o"></i> Email
+                  </span>{" "}
+                  {props.data ? props.data.email : "loading"}
+                </p>
+              </div>
+            </div>
+            <div className="col-md-12">
+              <div className="row">
+                <div className="social">
+                  <ul>
+                    <li>
+                      <a href={props.data ? props.data.facebook : "/"}>
+                        <i className="fa fa-facebook"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a href={props.data ? props.data.twitter : "/"}>
+                        <i className="fa fa-twitter"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a href={props.data ? props.data.youtube : "/"}>
+                        <i className="fa fa-youtube"></i>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <ToastContainer position="top-right" autoClose={5000} />
+        <div id="footer">
+          <div className="container text-center">
+            <p>&copy; 2023-2024 Startech Softwares. All Rights Reserved.</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
